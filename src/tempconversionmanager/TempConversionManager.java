d9b771b5e38a83983449300a48a2234aeee37c9c
AhmedEltorky <eng.ahmed.adel1991@gmail.com>
2018-09-30 00:51:48 +0200

    First commit.

---
+++ src/tempconversionmanager/TempConversionManager.java
@@ -0,0 +1,13 @@
+package tempconversionmanager;
+
+/**
+ *
+ * @author Ahmed El-Torky
+ */
+public class TempConversionManager {
+
+    public static void main(String[] args) {
+        View.main(args);
+    }
+    
+}
